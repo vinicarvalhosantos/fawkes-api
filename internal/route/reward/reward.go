eb431beec0d0653226c54a1b338fec31cd3841ca
Luiz Marcelo Pelizaro <luiz.pelizaro127@hotmail.com>
2022-02-11 18:05:42 -0300

    Created the functionality to get all rewards , get by id , change and delete rewards.

--- internal/route/reward/reward.go
+++ internal/route/reward/reward.go
@@ -9,8 +9,14 @@ import (
 func SetupRewardRoutes(router fiber.Router) {
 
 	rewardRoute := router.Group("/reward")
-
+	//Get all rewards
+	rewardRoute.Get("/", jwt.Protected(), reward.GetReward)
+	//Get reward by id
+	rewardRoute.Get("/:rewardId", jwt.Protected(), reward.GetRewardByID)
 	//Create a new Reward
 	rewardRoute.Post("/", jwt.Protected(), reward.RegisterReward)
-
+	//Update reward by id
+	rewardRoute.Put("/:rewardId", jwt.Protected(), reward.UpdateReward)
+	//Delete reward
+	rewardRoute.Delete("/:rewardId", jwt.Protected(), reward.DeleteReward)
 }
